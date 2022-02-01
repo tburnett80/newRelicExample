48f80c49239d0705dc32b649914a96583a61f670
Tim <tburnett80@gmail.com>
2022-02-01 12:57:01 -0600

    .

--- TaskScripts/1809/setup_newrelic_agent.ps1
+++ TaskScripts/1809/setup_newrelic_agent.ps1
@@ -4,13 +4,18 @@ Write-Host "### Installing New Relic Agent"
 Write-Host "#####################################################"
 Write-Host " "
 
-$fileName = 'install_nr.ps1'; `
-    Write-Host "## Downloading New Relic Agent"; `
-    (New-Object System.Net.WebClient).DownloadFile("https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1", $fileName); `
-    Write-Host "## Installing New Relic Agent"; `
-    Invoke-Expression -Command .\$fileName;
-    & Remove-Item $fileName;
-    & 'C:\Program Files\New Relic\New Relic CLI\newrelic.exe' install
+#$fileName = 'install_nr.ps1'; `
+#    Write-Host "## Downloading New Relic Agent"; `
+#    (New-Object System.Net.WebClient).DownloadFile("https://download.newrelic.com/install/newrelic-cli/scripts/install.ps1", $fileName); `
+#    Write-Host "## Installing New Relic Agent"; `
+#    Invoke-Expression -Command .\$fileName;
+#    & Remove-Item $fileName;
+#    & 'C:\Program Files\New Relic\New Relic CLI\newrelic.exe' install
+
+$LICENSE_KEY=[System.Environment]::GetEnvironmentVariable('NEW_RELIC_API_KEY', 'Machine'); `
+	(New-Object System.Net.WebClient).DownloadFile("https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.msi", "newrelic-infra.msi"); `
+	msiexec.exe /qn /i newrelic-infra.msi GENERATE_CONFIG=true LICENSE_KEY="$LICENSE_KEY" | Out-Null; `
+	net start newrelic-infra
 
 Write-Host " "
 Write-Host "## New Relic Agent Setup Complete!!!"
